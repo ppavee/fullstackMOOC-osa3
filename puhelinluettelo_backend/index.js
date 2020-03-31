8c77eb05400248d26ea0a5a6328ee38fb66f81d9
Paavo Rantala <paavo.rantala@kosila.fi>
2020-03-31 10:26:32 +0300

    puhelinluettelo backend step2

--- puhelinluettelo_backend/index.js
+++ puhelinluettelo_backend/index.js
@@ -28,6 +28,13 @@ app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+    const peopleCount = persons.length
+    const requestTime = new Date()
+    response.send(`<p>Phonebook has info for ${peopleCount} people</p>
+                   <p>${requestTime.toString()}</p>`)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`)
